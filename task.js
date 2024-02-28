9ac38811385e48ed0b231d67900dba285d031781
Alex Lumex <beketov.alex98@yandex.ru>
2024-02-28 14:44:05 +0300

    Add updateQuantity and calculateTotal

--- task.js
+++ task.js
@@ -27,10 +27,20 @@ const shoppingCart = {
   },
 
   updateQuantity(name, quantity) {
-    this.calculateTotal();
+    const item = this.items.find((i) => i.name === name); //нашли объект
+    if (item) {
+      //если он существует
+      item.quantity = quantity; //увеличили его количество
+      this.calculateTotal(); // и пересчитали общую стоимость
+    }
   },
 
-  calculateTotal() {},
+  calculateTotal() {
+    this.total = this.items.reduce(
+      (acc, item) => acc + item.price * item.quantity,
+      0
+    ); //редюсим на каждой итерации считая стоимость одного типа товара и складывая с аккумулятором
+  },
 
   clearCart() {
     //просто обнуляем все
@@ -38,3 +48,12 @@ const shoppingCart = {
     this.total = 0;
   },
 };
+
+shoppingCart.addItem({ name: "Apple", price: 1, quantity: 3 });
+shoppingCart.addItem({ name: "Banana", price: 0.5, quantity: 4 });
+console.log(shoppingCart.total);
+shoppingCart.removeItem("Apple");
+shoppingCart.updateQuantity("Banana", 2);
+console.log(shoppingCart.items);
+shoppingCart.clearCart();
+console.log(shoppingCart.items);
