3a8343bdb6711e9235eca8cf8bdfd17467971427
Alex Lumex <beketov.alex98@yandex.ru>
2024-02-28 14:31:47 +0300

    Add comments

--- task.js
+++ task.js
@@ -3,12 +3,16 @@ const shoppingCart = {
   total: 0,
 
   addItem(item) {
-    const { name, price, quantity } = item;
-    const existingItem = this.items.find((i) => i.name === name);
+    const { name, price, quantity } = item; //деструктуризируем объект из аргумента
+    const existingItem = this.items.find(
+      (i) => i.name === name
+    ); /*находим уже существующий товар в корзине 
+    и получаем его объект или не находим и получаем undefined; */
     if (existingItem) {
-      existingItem.quantity += quantity;
+      //тут проверка
+      existingItem.quantity += quantity; //если нашли, то просто увеличиваем количество этого товара
     } else {
-      this.items.push({ name, price, quantity });
+      this.items.push({ name, price, quantity }); //есди не нашли добавляем новый
     }
     this.calculateTotal();
   },
@@ -24,6 +28,7 @@ const shoppingCart = {
   calculateTotal() {},
 
   clearCart() {
+    //просто обнуляем все
     this.items = [];
     this.total = 0;
   },
